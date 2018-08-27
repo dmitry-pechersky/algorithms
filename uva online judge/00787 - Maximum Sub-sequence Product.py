5cd83c57499955c2c1a61ce52ece5119243ce73b
Dmitry Pechersky <whoisdmitry@gmail.com>
2018-08-27 17:40:30 +0300

    00787 - Maximum Sub-sequence Product

--- uva online judge/00787 - Maximum Sub-sequence Product.py
+++ uva online judge/00787 - Maximum Sub-sequence Product.py
@@ -8,12 +8,12 @@ def read_test_case():
 
 def max_subarray_product(array):
     n = len(array)
-    min_array, max_array = [0] * n, [0] * n
-    min_array[0], max_array[0] = array[0], array[0]
+    max_product, min_value, max_value = array[0], array[0], array[0]
     for i in range(1, n):
-        max_array[i] = max(max_array[i - 1] * array[i], min_array[i - 1] * array[i], array[i])
-        min_array[i] = min(max_array[i - 1] * array[i], min_array[i - 1] * array[i], array[i])
-    return max(max_array)
+        max_value, min_value = max(max_value * array[i], min_value * array[i], array[i]), min(max_value * array[i], min_value * array[i], array[i])
+        if max_value > max_product:
+            max_product = max_value
+    return max_product
 
 if __name__ == '__main__':
     for array in iter(read_test_case, None):
