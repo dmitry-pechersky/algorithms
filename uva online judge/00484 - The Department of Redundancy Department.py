a65ce6fda2ff682c59eaf22b734b52a1df8036e7
Dmitry <none@none.com>
2019-07-27 12:34:35 +0300

    uva 00484 - The Department of Redundancy Department

--- uva online judge/00484 - The Department of Redundancy Department.py
+++ uva online judge/00484 - The Department of Redundancy Department.py
@@ -1,7 +1,10 @@
+import sys
+
 nums, dic = [], {}
-for num in (int(i) for i in input().split()):
-    if num not in dic:
-        nums.append(num)
-    dic[num] = dic.get(num, 0) + 1
+for line in sys.stdin:
+    for num in (int(i) for i in line.split()):
+        if num not in dic:
+            nums.append(num)
+        dic[num] = dic.get(num, 0) + 1
 for num in nums:
     print(num, dic[num])
