02d7b412c8618ff0a379f9b8a94a8563cb57d313
Dmitry <none@none.com>
2019-07-31 22:18:36 +0300

    uva 00484 - The Department of Redundancy Department

---
+++ uva online judge/00484 - The Department of Redundancy Department.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <assert.h>
+#define HASH_SIZE 28151
+#define EMPTY -1
+
+int table[HASH_SIZE];
+int values[HASH_SIZE];
+int order[HASH_SIZE];
+
+int _hash(int key, int i){
+    return (((unsigned int) key) % HASH_SIZE + i * (1 + ((unsigned int) key) % (HASH_SIZE - 1))) % HASH_SIZE;
+}
+
+int _hash_insert(int table[], int key){
+    int i, hash;
+    for(i = 0; i < HASH_SIZE; i++){
+        hash = _hash(key, i);
+        if(table[hash] == EMPTY){
+            table[hash] = key;
+            return hash;
+        }
+    }
+    assert(0);
+}
+
+int _hash_search(int table[], int key){
+    int i, hash;
+    for(i = 0; i < HASH_SIZE; i++){
+        hash = _hash(key, i);
+        if(table[hash] == key){
+            return hash;
+        }else if(table[hash] == EMPTY){
+            break;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int i, j, value, values_cnt = 0;
+    for(i = 0; i < HASH_SIZE; i++){
+        table[i] = EMPTY;
+    }
+    while(scanf("%d", &value) > 0){
+        j = _hash_search(table, value);
+        if(j >= 0){
+            values[j] += 1;
+        }else{
+            j = _hash_insert(table, value);
+            values[j] = 1;
+            order[values_cnt++] = value;
+        }
+    }
+    for(i = 0; i < values_cnt; i++){
+        printf("%d %d\n", order[i], values[_hash_search(table, order[i])]);
+    }
+
+    return 0;
+}
