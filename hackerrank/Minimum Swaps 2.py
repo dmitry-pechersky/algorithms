c124eabb701a134da1ff5eca602528a3aea9d2e6
Dmitry <none@none.com>
2019-08-07 11:14:32 +0300

    Minimum Swaps 2

--- hackerrank/Minimum Swaps 2.py
+++ hackerrank/Minimum Swaps 2.py
@@ -1,12 +1,9 @@
 if __name__ == '__main__':
     n, nums = int(input()), [int(i) - 1 for i in input().split()]
-    positions = [0] * n
-    for i in range(n):
-        positions[nums[i]] = i
     swaps = 0
     for i in range(n):
-        if i != nums[i]:
-            nums[positions[i]] = nums[i]
-            positions[nums[i]] = positions[i]
+        while nums[i] != i:
+            tmp = nums[i]
+            nums[i], nums[tmp] = nums[tmp], tmp
             swaps += 1
     print(swaps)
