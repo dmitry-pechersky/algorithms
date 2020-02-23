0df1839bb7b200cf70b06aa1153a2287984a3b3a
Dmitry Pechersky <whoisdmitry@gmail.com>
2020-02-23 10:39:48 +0300

    Knapsacks

--- hackerearth/Knapsacks.py
+++ hackerearth/Knapsacks.py
@@ -1,10 +1,10 @@
 def dp_knapsack_big_weights(n, c, costs, weights):
     max_cost = 0
     total_cost = sum(costs)
-    dp_cur = [INF] * (total_cost + 1)
+    dp_cur = [float('Inf')] * (total_cost + 1)
     for i in range(n):
         dp_prev = dp_cur
-        dp_cur = [INF] * (total_cost + 1)
+        dp_cur = [float('Inf')] * (total_cost + 1)
         for cost in range(1, total_cost + 1):
             if costs[i] >= cost:
                 dp_cur[cost] = weights[i] if dp_prev[cost] > weights[i] else dp_prev[cost]
@@ -15,7 +15,7 @@ def dp_knapsack_big_weights(n, c, costs, weights):
             if dp_cur[cost] > c:
                 break
     return max_cost
-
+ 
 if __name__ == '__main__':
     n, c = (int(i) for i in input().split())
     costs = [int(i) for i in input().split()]
